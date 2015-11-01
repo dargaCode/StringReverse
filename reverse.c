06a7318a76332dadac3cf4e3690ef034f9e12173
dargaCode <darga.code@gmail.com>
2015-11-01 00:00:07 -0400

    removed debug statements

--- reverse.c
+++ reverse.c
@@ -6,12 +6,11 @@ void reverse(char*);
 
 int main(void)
 {
-    printf("Enter string to reverse: ");
+    printf("\nEnter string to reverse: ");
     char* input = GetString();
-    printf("Reversing string \"%s\"...\n", input);
-
+    printf("\n\nReversing string \"%s\"...\n", input);
     reverse(input);
-    printf("Reversed string: %s \n", input);
+    printf(" Reversed string: %s \n\n", input);
 }
 
 void reverse(char* str)
@@ -19,8 +18,6 @@ void reverse(char* str)
     int len = strlen(str);
     for (int i = 0, j = i + len - 1; i < j; i++, j--)
     {
-        printf("i: %i str[i]: %c j: %i str[j]: %c \n",
-            i, *(str+i), j, *(str+j));
         char temp = *(str + i);
         *(str + i) = *(str + j);
         *(str + j) = temp;
