cd18a1a6948af59703059345757e1973ceab3cd4
dargaCode <darga.code@gmail.com>
2015-10-31 23:01:00 -0400

    initial commit

---
+++ reverse.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+void reverse(char*);
+
+int main(void)
+{
+    char* input = "Hello, world!";
+    printf("%s \n", input);
+    int len = strlen(input);
+    printf("%i \n", len);
+}
+
+void reverse(char* str)
+{
+
+}
